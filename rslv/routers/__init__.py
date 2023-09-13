bc68aff3bb665ff322941329ad7282d489303b05
datadave <605409+datadavev@users.noreply.github.com>
2023-09-12 21:55:17 -0400

    Pretty print json output

---
+++ rslv/routers/__init__.py
@@ -0,0 +1,14 @@
+import json
+import typing
+import starlette.responses
+class PrettyJSONResponse(starlette.responses.Response):
+    media_type = "application/json"
+
+    def render(self, content: typing.Any) -> bytes:
+        return json.dumps(
+            content,
+            ensure_ascii=False,
+            allow_nan=False,
+            indent=2,
+            separators=(", ", ": "),
+        ).encode("utf-8")
